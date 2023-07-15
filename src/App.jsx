749320ac6ac487a1471b3ce6a28c4cb72aec777a
hari88018 <harikrishna88018@gmail.com>
2023-07-15 23:31:36 +0530

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
+import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Navbar from "../src/Components/Navbar/Navbar";
 
-import ProductsList from './Pages/ProductsList';
-import Home from "./Pages/Home/Home"
-import Product from './Pages/Product';
-import Register from './Pages/Register/Register';
-import Signin from './Pages/SignIn/Signin';
-import Cart from './Pages/CART/Cart';
-import ResetPassword from './Pages/ResetPassword/ResetPassword';
+import ProductsList from "./Pages/ProductsList";
+import Home from "./Pages/Home/Home";
+import Product from "./Pages/Product";
+import Register from "./Pages/Register/Register";
+import Signin from "./Pages/SignIn/Signin";
+import Cart from "./Pages/CART/Cart";
+import ResetPassword from "./Pages/ResetPassword/ResetPassword";
 
 const App = () => {
   return (
@@ -17,14 +17,10 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/Register" element={<Register />}></Route>
         <Route path="/Signin" element={<Signin />}></Route>
-        {/* <Route path="/Cart" element={<Cart />}></Route> */}
-
-        
-
-      
+        <Route path="/Cart" element={<Cart />}></Route>
       </Routes>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
